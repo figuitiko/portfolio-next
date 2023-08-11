92818cfeea91c753f0e002f20d994bb52ee910db
frank renato <frankfigao@gmail.com>
2023-08-10 18:12:54 -0600

    Fixing shaking

--- src/components/share/CommonShape.jsx
+++ src/components/share/CommonShape.jsx
@@ -2,7 +2,7 @@
 
 const CommonShape = ({ children, classCustom }) => {
   return (
-    <div className={`flex  bg-[linear-gradient(to_bottom,#1f1e1e,#272626,#302f2f,#393838,#424141)] dark:bg-[linear-gradient(to_right,#f6f6f6,#f5f7f8,#f3f8f9,#f3f9f6,#f7f9f2)] hover:scale-105 ease-in-out duration-300 rounded-2xl w-[100px] h-[100px] p-4 gap-1 dark:text-slate-700   ${classCustom}`} >
+    <div className={`flex  bg-[linear-gradient(to_bottom,#1f1e1e,#272626,#302f2f,#393838,#424141)] dark:bg-[linear-gradient(to_right,#f6f6f6,#f5f7f8,#f3f8f9,#f3f9f6,#f7f9f2)] hover:scale-105 ease-in-out rounded-2xl w-[100px] h-[100px] p-4 gap-1 dark:text-slate-700   ${classCustom}`} >
       {children}
     </div>
   )
