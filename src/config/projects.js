9bdc8c904925ff92277500a4fff56c5d554f42f4
frank renato <frankfigao@gmail.com>
2023-08-12 21:02:45 -0600

    Fixing ip tracker path

--- src/config/projects.js
+++ src/config/projects.js
@@ -44,7 +44,7 @@ export const rightSide = [
       classCustom: 'block-about-first-line block-project-first-line',
       children: (
         <ProjectItem
-          imgName='Ip-tracker.png'
+          imgName='ip-tracker.png'
           width={200} height={220}
           textInfo={'Ip tracker'}
           isClickAble={true}
